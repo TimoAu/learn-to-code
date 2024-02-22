86e0f9e4071e351c734b232a55638317745d96b8
TimoAu <156666237+TimoAu@users.noreply.github.com>
2024-02-22 12:07:31 +0100

    komisches cmake debugger zeug

---
+++ Uebungen/cmake.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    printf("Hello World");
+    int vara = 15;
+    int varb = 30;
+    int varc = 20;
+    vara = vara+varc;
+    varb = vara-varc;
+    double vard = 1.734798;
+    double vare = 0.0;
+    vare = varb*vard;
+    return 0;
+}

c1602388eed9e305b39c06fdf3999486835dfe01
TimoAu <156666237+TimoAu@users.noreply.github.com>
2024-03-04 19:10:26 +0100

    Hausaufgabe 1.4

---
+++ Hausaufgaben/Hausaufgabe_1.4/Student_Struct/CMakeFiles/student_struct_exec.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/learn-to-code/Hausaufgaben/Hausaufgabe_1.4/Student_Struct/Student.c" "CMakeFiles/student_struct_exec.dir/Student.c.o" "gcc" "CMakeFiles/student_struct_exec.dir/Student.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Hausaufgaben/Hausaufgabe_1.4/Student_Struct/CMakeFiles/student_struct_exec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student_struct_exec.dir/Student.c.o"
+  "CMakeFiles/student_struct_exec.dir/Student.c.o.d"
+  "student_struct_exec"
+  "student_struct_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/student_struct_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

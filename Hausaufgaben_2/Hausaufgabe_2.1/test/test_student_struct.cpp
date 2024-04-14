3703bc8808ca5edfd283df79adfb1a6d2c784d2e
TimoAu <156666237+TimoAu@users.noreply.github.com>
2024-04-14 15:21:53 +0200

    .

--- Hausaufgaben_2/Hausaufgabe_2.1/test/test_student_struct.cpp
+++ Hausaufgaben_2/Hausaufgabe_2.1/test/test_student_struct.cpp
@@ -3,5 +3,5 @@ extern"C"{
     #include"student.h"
 }
 TEST(tdd_tests, student_init ){
-EXPECT_EQ("Max Mustermann", student_init(&max, "Max Mustermann"));
+ASSERT_STREQ("Max Mustermann", max.name);
 }
